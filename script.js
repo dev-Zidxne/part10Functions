f9266408e83811a138dbfc1b9c5e65fff595f830
Zidane Innis <zidaneinnis@gmail.com>
2022-05-10 12:31:40 -0500

    Functions Accepting Callback Functions
    Understanding this is crucial
    
    One one the most important lectures
    
    These are a vital part of the JavaScript language

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 // Default Parameters
 /////////////////////////////////////////
 
@@ -29,3 +29,66 @@ createBooking('LH123', 2);
 createBooking('LH123', 5);
 
 createBooking('LH123', undefined, 1000);
+
+
+const flight = 'LH234';
+const zidane = {
+  name: 'Zidane Innis',
+  passport: 2475982749,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr.' + passenger.name;
+  if (passenger.passport === 2475982749) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+// checkIn(flight, zidane);
+// console.log(flight);
+// console.log(zidane);
+
+// Is tghe same as doing:
+// const flightNum = flight;
+// const passenger = zidane;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 10000000000000);
+};
+
+newPassport(zidane);
+checkIn(flight, zidane);
+*/
+
+// Functions Accepting Callback Functions
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by : ${fn.name}`);
+};
+
+transformer('Javascript is the best!', upperFirstWord);
+transformer('Javascript is the best!', oneWord);
+
+// Jvascript uses callbacks ALL THE TIME
+const high5 = function () {
+  console.log('Waves');
+};
+
+document.body.addEventListener('click', high5);
+
+['Jonas', 'Martha', 'Adam'].forEach(high5);
